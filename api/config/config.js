5b29219e5fb6f958927fc55a87a4be0ea8e77b8c
valenante <valentinoantenucci1@gmail.com>
2025-04-22 16:29:26 +0200

    Enviar al carrito y al pedido si el plato es tapa, ración o único

--- api/config/config.js
+++ api/config/config.js
@@ -22,7 +22,10 @@ export const corsOptions = {
     "http://192.168.98.203:3001",
     "http://192.168.98.203:3002",
     "http://192.168.1.142:3001",
-    "http://192.168.1.142:3002"
+    "http://192.168.1.142:3002",
+    "http://192.168.18.26:3001",
+    "http://192.168.18.26:3000",
+    "http://192.168.18.26:3002"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Cart-ID"],
