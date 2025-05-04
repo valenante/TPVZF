b5f8f126dcf378a0a72e4197fd96477fe3868d7d
Valentino Antenucci <valentinoantenucci1@gmail.com>
2025-05-05 01:08:43 +0200

    Traducciones PreMenu

--- carta/src/App.js
+++ carta/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ProductosProvider } from './context/ProductosContext';
 import { LanguageProvider } from "./context/LanguageContext"; // 👈 Importamos el contexto
-import { ToastContainer } from "react-toastify";
 import CartaPage from './pages/CartaPage';
 import PreMenu from './components/PreMenu/PreMenu';
 import Valoraciones from './pages/Valoraciones.js';
@@ -25,6 +24,7 @@ function App() {
   // Cargar idioma desde localStorage o usar "es" por defecto
   const storedLocale = localStorage.getItem("locale") || "es";
   const [locale, setLocale] = useState(storedLocale);
+  
 
   useEffect(() => {
     // Cargar mensajes y activar idioma
